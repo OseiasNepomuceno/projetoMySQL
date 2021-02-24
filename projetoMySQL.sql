d95029468a1d0b942ce3645aab9d798bb733a7e7
Oseias Nepomuceno <oseiasnepom@gmail.com>
2021-02-24 02:29:07 -0300

    inserindo dados

--- projetoMySQL.sql
+++ projetoMySQL.sql
@@ -4,3 +4,8 @@ CREATE TABLE pessoa (
     nascimento DATE
 )
 
+INSERT INTO pessoa (nome, nascimento) VALUES ('Oseias', '1977-10-14')
+INSERT INTO pessoa (nome, nascimento) VALUES ('Eliane', '1973-11-01')
+INSERT INTO pessoa (nome, nascimento) VALUES ('Tereza', '1952-10-22')
+INSERT INTO pessoa (nome, nascimento) VALUES ('Eliseu', '1951-12-26')
+
